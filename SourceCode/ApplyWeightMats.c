41aefc3974efb601610830acc7a0658d6ad6eabe
AliFarghadan <161867326+AliFarghadan@users.noreply.github.com>
2025-01-31 00:29:59 -0500

    Update ApplyWeightMats.c

--- SourceCode/ApplyWeightMats.c
+++ SourceCode/ApplyWeightMats.c
@@ -22,6 +22,7 @@ PetscErrorCode ApplyWeightMats(RSVD_matrices *RSVD, RSVDt_vars *RSVDt, Weight_ma
 			}
 			if (Weight->InputMatrixFlg)  {
 				ierr = MatMatMult(Weight->B,RSVD->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
+				ierr = MatDestroy(&RSVD->Y_hat);CHKERRQ(ierr);
 				ierr = MatDuplicate(Y,MAT_COPY_VALUES,&RSVD->Y_hat);CHKERRQ(ierr);
 				ierr = MatDestroy(&Y);CHKERRQ(ierr);
 			}
